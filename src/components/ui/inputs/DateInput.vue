a79cd38580fb05d3ff4ebe62d6636c9efffe858c
SaiKG <saikumar.gurugu.com>
2025-05-19 22:19:19 +1000

    FIll IN

--- src/components/ui/inputs/DateInput.vue
+++ src/components/ui/inputs/DateInput.vue
@@ -1,85 +1,97 @@
 <template>
-    <v-menu v-model="menu" transition="scale-transition" offset-y>
-      <!-- Remove the debug line "{{ activatorProps }}" -->
-      <template v-slot:activator="{ props: activatorProps }">
-        <v-text-field
-          v-bind="activatorProps"
-          v-model="displayDate"
-          :label="config.label || 'Select Date'"
-          :rules="rules"
-          readonly
-          outlined
-          density="comfortable"
-          class="input-control"
-          @click="menu = true"
+    <div class="date-picker-wrapper">
+      <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        transition="scale-transition"
+        offset-y
+        min-width="290px"
+      >
+        <template v-slot:activator="{ props }">
+          <v-text-field
+            v-bind="props"
+            :model-value="formattedDate"
+            label="Pick a date"
+            readonly
+            variant="outlined"
+            density="comfortable"
+            class="input-field"
+            color="primary"
+            base-color="primary"
+            hide-details
+          />
+        </template>
+        <v-date-picker
+          v-model="internalDate"
+          @change="onDateSelected"
+          color="primary"
+          class="date-picker"
+          show-adjacent-months
         />
-      </template>
-      <v-date-picker
-        v-model="internalValue"
-        @change="menu = true"
-        :max="config.attrs?.max"
-        :min="config.attrs?.min"
-        persistent-placeholder
-        class="input-control"
-      />
-      <!-- Optionally remove or place this date input elsewhere unless required -->
-      <v-date-input
-        label="Date of birth"
-        prepend-icon=""
-        variant="outlined"
-        persistent-placeholder
-      ></v-date-input>
-    </v-menu>
+      </v-menu>
+    </div>
   </template>
   
-  <script setup>
-  import { computed, ref } from 'vue'
-  
-  const props = defineProps({
-    config: {
-      type: Object,
-      default: () => ({})
+  <script>
+  export default {
+    name: "DatePickerInput",
+    props: {
+      modelValue: {
+        type: String,
+        default: "",
+      },
     },
-    modelValue: {
-      type: [String, Number, Boolean, Array, Object],
-      default: ''
+    data() {
+      return {
+        menu: false,
+        internalDate: this.modelValue || new Date().toISOString().split("T")[0],
+      };
     },
-    rules: {
-      type: Array,
-      default: () => []
-    }
-  })
-  const emit = defineEmits(['update:modelValue'])
-  
-  const internalValue = computed({
-    get: () => props.modelValue,
-    set: (val) => emit('update:modelValue', val)
-  })
-  
-  const menu = ref(false)
-  
-  const displayDate = computed({
-    get() {
-      if (!internalValue.value) return ''
-      const date = new Date(internalValue.value)
-      return isNaN(date.getTime())
-        ? internalValue.value
-        : date.toLocaleDateString()
+    watch: {
+      modelValue(newVal) {
+        this.internalDate = newVal;
+      },
     },
-    set(newDisplay) {
-      const parsedDate = new Date(newDisplay)
-      if (!isNaN(parsedDate.getTime())) {
-        internalValue.value = parsedDate.toISOString().split('T')[0]
-      } else if (!newDisplay) {
-        internalValue.value = ''
-      }
-    }
-  })
+    computed: {
+      formattedDate() {
+        if (!this.internalDate) return "";
+        const date = new Date(this.internalDate);
+        return isNaN(date.getTime())
+          ? this.internalDate
+          : date.toLocaleDateString();
+      },
+    },
+    methods: {
+      onDateSelected(value) {
+        this.menu = false;
+        this.internalDate = value;
+        this.$emit("update:modelValue", value);
+      },
+    },
+  };
   </script>
   
   <style scoped>
-  .input-control {
+  .date-picker-wrapper {
     width: 100%;
+    margin-bottom: 1.5rem;
+  }
+  
+  .input-field {
+    width: 100%;
+    background-color: rgb(0, 0, 0);
+    border-radius: 8px;
+  }
+  
+  .input-field input {
+    color: black !important; /* force visible text */
+    caret-color: black !important;
+    /* background-color: white !important; */
+  }
+  
+  .date-picker {
+    padding: 1rem;
+    border-radius: 8px;
   }
   </style>
   
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,5 +6,33 @@ const vuetify = createVuetify({
   ssr: true,
   Comments:{
     VDateInput,
-  }
+  },
+  theme: {
+    defaultTheme: 'light', // 👈 set this to 'light'
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          background: '#FFFFFF',
+          surface: '#FFFFFF',
+          primary: '#1976D2',
+          secondary: '#424242',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FB8C00',
+        },
+      },
+      dark: {
+        dark: true,
+        colors: {
+          background: '#121212',
+          surface: '#1E1E1E',
+        },
+      },
+    },
+  },
+
 })
+
+export default vuetify;
